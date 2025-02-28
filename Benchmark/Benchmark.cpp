94be3fb0ef8e209a21fd73e98aaab8480cecded7
aks7 <aks7@alu.ua.es>
2025-02-28 09:57:41 +0100

    v0.1

--- Benchmark/Benchmark.cpp
+++ Benchmark/Benchmark.cpp
@@ -1,22 +1,22 @@
-// Benchmark.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
-#define N 512  // Tamaño de la matriz
+#define SAMPLES 3
+#define MAX_SIZE 1000
 
-// Inicializa una matriz con valores aleatorios
-void initialize_matrix(int matrix[N][N]) {
+int A[MAX_SIZE][MAX_SIZE], B[MAX_SIZE][MAX_SIZE], C[MAX_SIZE][MAX_SIZE];
+
+void initialize_matrix(int matrix[MAX_SIZE][MAX_SIZE], int N) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            matrix[i][j] = rand() % 10; // Valores entre 0 y 9
+            matrix[i][j] = rand() % 10;
         }
     }
 }
 
-// Multiplicación de matrices
-void multiply_matrices(int A[N][N], int B[N][N], int C[N][N]) {
+void multiply_matrices(int N) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             C[i][j] = 0;
@@ -27,37 +27,78 @@ void multiply_matrices(int A[N][N], int B[N][N], int C[N][N]) {
     }
 }
 
-void print_matrices(int X[N][N]) {
+void print_matrix(int matrix[MAX_SIZE][MAX_SIZE], int N) {
+    printf("Matriz (%dx%d):\n", N, N);
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            printf("%d ", X[i][j]);
+            printf("%2d ", matrix[i][j]); // %2d para alineación bonita
         }
         printf("\n");
+    }
+    printf("\n");
+}
+
+void multiply_matrices_asm(int N) {
+    __asm {
+        mov ecx, N
+        xor esi, esi
+        loop_i :
+        xor edi, edi
+            loop_j :
+        xor eax, eax
+            xor edx, edx
+            loop_k :
+        mov ebx, esi
+            imul ebx, N
+            add ebx, edx
+            mov ebx, [A + ebx * 4]
+
+            mov edi, edx
+            imul edi, N
+            add edi, ecx
+            mov edi, [B + edi * 4]
+
+            imul ebx, edi
+            add eax, ebx
+
+            inc edx
+            cmp edx, N
+            jl loop_k
+
+            mov ebx, esi
+            imul ebx, N
+            add ebx, edi
+            mov[C + ebx * 4], eax
+
+            inc edi
+            cmp edi, N
+            jl loop_j
+
+            inc esi
+            cmp esi, N
+            jl loop_i
     }
 }
 
 int main() {
-    static int A[N][N], B[N][N], C[N][N];
     srand(time(NULL));
 
-    initialize_matrix(A);
-    initialize_matrix(B);
+    for (int i = 0; i < SAMPLES; i++) {
+        int N = 1000;
 
-    clock_t start = clock();
-    multiply_matrices(A, B, C);
-    clock_t end = clock();
+        clock_t start, end;
+        initialize_matrix(A, N);
+        initialize_matrix(B, N);
 
-    //print_matrices(A);
-    //printf("\n");
-    //print_matrices(B);
-    //printf("\n");
-    //print_matrices(C);
-    // print_matrices(B);
-    // print_matrices(C);
-    
+        start = clock();
+        multiply_matrices(N);
+        end = clock();
+        printf("C - Tiempo: %.3f s\n", (double)(end - start) / CLOCKS_PER_SEC);
 
-    double time_taken = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("Tiempo de ejecución: %.6f segundos\n", time_taken);
-
+        start = clock();
+        multiply_matrices_asm(N);
+        end = clock();
+        printf("ASM - Tiempo: %.3f s\n", (double)(end - start) / CLOCKS_PER_SEC);
+    }
     return 0;
 }
